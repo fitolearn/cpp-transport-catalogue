33123f148426757bbb09c167529ae59e3044df2d
fitolearn <112857538+fitolearn@users.noreply.github.com>
2023-05-09 13:18:30 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -2,24 +2,29 @@
 
 #include <cmath>
 
-struct Coordinates {
-    double lat;
-    double lng;
-    bool operator==(const Coordinates& other) const {
-        return lat == other.lat && lng == other.lng;
-    }
-    bool operator!=(const Coordinates& other) const {
-        return !(*this == other);
-    }
-};
+namespace geo {
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
-    using namespace std;
-    if (from == to) {
-        return 0;
+    struct Coordinates {
+        double latitude;
+        double longitude;
+        bool operator==(const Coordinates& other) const {
+            return latitude == other.latitude && longitude == other.longitude;
+        }
+        bool operator!=(const Coordinates& other) const {
+            return !(latitude == other.latitude && longitude == other.longitude);
+        }
+    };
+
+    inline double ComputeDistance(Coordinates from, Coordinates to) {
+        using namespace std;
+        if (from == to) {
+            return 0;
+        }
+        static const double DIAMETER = 3.1415926535 / 180.;
+        static const int EARTH_RADIUS = 6371000;
+        return acos(sin(from.latitude * DIAMETER) * sin(to.latitude * DIAMETER)
+                    + cos(from.latitude * DIAMETER) * cos(to.latitude * DIAMETER) * cos(abs(from.longitude - to.longitude) * DIAMETER))
+               * EARTH_RADIUS;
     }
-    static const double dr = 3.1415926535 / 180.;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
-}
+
+} // namespace geo
