a0230738cb29d757895f384274dbda5c95feb971
fitolearn <112857538+fitolearn@users.noreply.github.com>
2023-05-09 13:33:33 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,16 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+namespace transport {
+
+    void ProcessRequests(Catalogue& catalogue);
+
+    namespace detail {
+
+        void PrintRoute(std::string& line, Catalogue& catalogue);
+        void PrintStop(std::string& line, Catalogue& catalogue);
+
+    } // namespace detail
+
+} // namespace transport
