103a4af317ac6d6ffdd6264e996a7e1175e66b15
shishka <112857538+fitolearn@users.noreply.github.com>
2023-05-16 21:57:29 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,56 +1,74 @@
 #pragma once
-
 #include "geo.h"
-
-#include <iostream>
-#include <deque>
-#include <string>
-#include <unordered_map>
 #include <vector>
-#include <stdexcept>
-#include <optional>
+#include <deque>
+#include <unordered_map>
+#include <string>
+#include <string_view>
 #include <unordered_set>
+#include <iostream>
 #include <set>
+#include <optional>
 
-namespace transport {
+namespace transport_catalogue
+{
+	struct Stop
+	{
+		std::string name;
+		geo::Coordinates coordinates;
+	};
 
-    struct Bus {
-        std::string number;
-        std::vector<std::string> stops;
-        bool circular_route;
-    };
+	struct Bus
+	{
+		std::string name;
+		std::vector<std::string_view> stops;
+		bool is_roundtrip;
+	};
 
-    struct Stop {
-        std::string name;
-        geo::Coordinates coordinates;
-        std::set<std::string> buses;
-        std::unordered_map<std::string, int> stop_distances;
-    };
+	struct BusInfo
+	{
+		std::string name;
+		int amount_stops;
+		int uniq_stops;
+		double route_length;
+		double curvature;
+	};
 
-    struct RouteInfo {
-        size_t stops_count;
-        size_t unique_stops_count;
-        double route_length;
-        double curvature;
-    };
+	namespace detail
+	{
+		struct PairStopHasher
+		{
+			std::size_t operator()(const std::pair<const  Stop*, const  Stop*>& pair_stop) const;
+		};
+	}// namespace detail
 
-    class Catalogue {
-    public:
-        void AddRoute(Bus& bus);
-        void AddStop(Stop& stop);
-        const Bus* FindRoute(const std::string& route_number) const;
-        Stop* FindStop(const std::string& stop_name) const;
-        const RouteInfo RouteInformation(const std::string& route_number) const;
-        size_t UniqueStopsCount(const std::string& route_number) const;
-        const std::set<std::string> GetBusesOnStop(const std::string& stop_name) const;
-        void SetDistance(Stop* from, Stop* to, int distance);
-        int GetDistance(const Stop* from, const Stop* to) const;
+	class TransportCatalogue
+	{
+	public:
+		void AddStop(Stop stop);
 
-    private:
-        std::deque<Bus> all_buses_;
-        std::deque<Stop> all_stops_;
-        std::unordered_map<std::string_view, const Bus*> busname_to_bus_;
-        std::unordered_map<std::string_view, Stop*> stopname_to_stop_;
-    };
+		const Stop* FindStop(const std::string_view stop) const;
 
-} // namespace transport
+		std::optional<std::set<std::string_view>> GetStopBuses(std::string_view stop) const;
+
+		void SetDistance(const std::string& stop, std::vector<std::pair<std::string, int>>& distances_to_stops);
+
+		int GetDistance(const Stop* stop_ptr, const Stop* anoter_stop_ptr) const;
+
+		void AddBus(const std::tuple<std::string, bool, std::vector<std::string>>& bus_input);
+
+		const Bus* FindBus(std::string_view bus) const;
+
+		std::optional<BusInfo> GetBusInfo(std::string_view bus) const;
+
+	private:
+		std::deque<Bus> buses_;
+		std::unordered_map<std::string_view, Bus*> busname_to_bus_;
+
+		std::deque<Stop> stops_;
+		std::unordered_map<std::string_view, Stop*> stopname_to_stop_;
+		std::unordered_map<std::string_view, std::set<std::string_view>> stopname_to_busnames_;
+
+		std::unordered_map<std::pair<const Stop*, const Stop*>, int, detail::PairStopHasher> stops_distances_;
+	};
+}//namespace transport_catalogue
